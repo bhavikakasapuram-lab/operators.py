2b48b5c7466968072d1f15edc87e9cad87041007
bhavikakasapuram-lab <bhavikakasapuram@gmail.com>
2025-09-16 15:26:38 +0530

    Update and rename README.md to operators.py

---
+++ operators.py
@@ -0,0 +1,38 @@
+# operators.py
+# =, -,*,/,**, //
+
+a = 10
+b = 20
+
+# print(a + b)
+# print(a - b)
+# print(10 ** 3)
+print(10 // 3)
+
+sum = 30
+sum += 10
+print(sum)
+sum = 10 
+sum -= 20
+print(sum)
+sum = 30
+sum *= 12
+print(sum)
+sum = 12
+sum /= 3
+print(sum)
+sum = 10
+sum **= 30
+print(sum)
+sum = 30
+sum //= 40
+print(sum)
+
+ # comparsion operator
+ 
+a = 10
+b = 30
+
+# print(a == b)
+# print(a != b)
+# print(a > b)
